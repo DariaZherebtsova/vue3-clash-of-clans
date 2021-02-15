94a3a25ce1651c95b8ff02cbd8800cb36cd7cf41
Vedegis Evgenii <tichoo@yandex.ru>
2021-02-15 12:54:39 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="wapper">
+    <Header :title="title" :links="links" />
+    <div class="wrapper-content">
+      <div class="container">
+        <h1>Hello world</h1>
+      </div>
+    </div>
+    <Footer :links="links" />
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header";
+import Footer from "@/components/Footer";
+
+export default {
+  components: {
+    Header,
+    Footer,
+  },
+  data() {
+    return {
+      title: "Template",
+      links: [
+        {
+          title: "Home",
+          alias: "home",
+          url: "/",
+        },
+        {
+          title: "About",
+          alias: "about",
+          url: "/about",
+        },
+      ],
+    };
+  },
+};
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,39 @@
+<template>
+  <header>
+    <div class="navbar">
+      <div class="container">
+        <div class="navbar-content">
+          <a class="navbar-logo" href="/">{{ title }}</a>
+          <ul class="navbar-list">
+            <li class="navbar-item" v-for="link in links" :key="link.alias">
+              <a class="navbar-link" :href="link.url">{{
+                link.title
+              }}</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    links: {
+      type: Array,
+      default: [
+        {
+          title: "Home",
+          alias: "home",
+          url: "/",
+        },
+      ],
+    },
+  },
+};
+</script>
