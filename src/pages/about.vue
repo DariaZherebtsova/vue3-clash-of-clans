c801b270a63089557cad64e327ff95f7280176db
Evgenii Vedegis <tichoo@yandex.ru>
2021-02-15 23:08:07 +0800

    added router and pages

---
+++ src/pages/about.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="md-body">
+    <div class="view-sm isCenter">
+      <h1>В этом приложении:</h1>
+      <ul>
+        <li>бла бла</li>
+        <li>бла бла</li>
+        <li>бла бла</li>
+      </ul>
+      <p>Сделано на курсе <a href="#">Vue 3</a></p>
+    </div>
+  </div>
+</template>
