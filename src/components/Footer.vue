7ff8ba84051cfff707fb56ac757154a341b4259a
Evgenii Vedegis <tichoo@yandex.ru>
2021-02-17 15:41:50 +0800

    asd

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -6,32 +6,18 @@
           <a class="navbar-link" :href="link.url">{{ link.title }}</a>
         </li>
       </ul>
-
-      <!-- link to course -->
-      <ul class="navbar-list" style="margin-top: 14px;">
-        <li class="navbar-item" style="opacity: 0.6; font-size: 14px;">
-          <a href="https://tocode.ru" class="navbar-link"
-            >Сделано на курсе "Vue.js 3"</a
-          >
-        </li>
-      </ul>
     </div>
   </footer>
 </template>
 
 <script>
+import {links} from "@/_config";
+
 export default {
-  props: {
-    links: {
-      type: Array,
-      default: [
-        {
-          title: "Home",
-          alias: "home",
-          url: "/",
-        },
-      ],
-    },
+  data() {
+    return {
+      links: links,
+    };
   },
 };
 </script>
--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,9 +6,7 @@
           <a class="navbar-logo" href="/">{{ title }}</a>
           <ul class="navbar-list">
             <li class="navbar-item" v-for="link in links" :key="link.alias">
-              <a class="navbar-link" :href="link.url">{{
-                link.title
-              }}</a>
+              <a class="navbar-link" :href="link.url">{{ link.title }}</a>
             </li>
           </ul>
         </div>
@@ -18,22 +16,14 @@
 </template>
 
 <script>
+import {app, links} from "@/_config";
+
 export default {
-  props: {
-    title: {
-      type: String,
-      required: true,
-    },
-    links: {
-      type: Array,
-      default: [
-        {
-          title: "Home",
-          alias: "home",
-          url: "/",
-        },
-      ],
-    },
+  data() {
+    return {
+      title: app.title,
+      links: links,
+    };
   },
 };
 </script>
