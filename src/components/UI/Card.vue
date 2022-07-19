5f5ba0aa56ed24887b8bb2974617f582c89a5a98
zherebtsova-dd <zherebtsova-dd@ranepa.ru>
2022-07-19 09:34:28 +0300

    add Card

---
+++ src/components/UI/Card.vue
@@ -0,0 +1,26 @@
+<template lang="">
+  <div class="card">
+    <span class="card-name">{{ name }}</span>
+    <span class="card-title">{{ title }}</span>
+    <div class="card-body">card-body</div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: {
+      type: String,
+      require: true,
+    },
+    title: {
+      type: String,
+      require: true,
+    },
+  }
+}
+</script>
+
+<style lang="">
+  
+</style>
